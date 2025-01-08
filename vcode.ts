14843b0c040be82ca316d4241ffe149e5252a2d6
hghrktkr <147295919+hghrktkr@users.noreply.github.com>
2025-01-08 12:14:44 +0900

    アイコンの色指定

--- vcode.ts
+++ vcode.ts
@@ -4,7 +4,7 @@ namespace vcodeCustomBlocks {
      * @param blockType the block to place
      * @param startPosition the position to place the block
      */
-    //% block="fill with 4 blocks"
+    //% block="fill with 4 blocks" weight=200 color=#0B3D91 icon="\uf186"
     export function fillWith4Blocks(startPosition: Position): void {
         blocks.fill(
             IRON_BLOCK,
